0f33e8ea847e6fbeb79d0c340032dad4e590fca9
KarlChuang <karl0930@gmail.com>
2017-05-08 14:59:53 +0800

    user name autoFill after first submit

--- src/Text.js
+++ src/Text.js
@@ -25,7 +25,6 @@ class Text extends Component {
       if (this.state.contentValue !== "" && this.state.nameValue !== "") {
         this.props.onClick(this.state.nameValue, this.state.contentValue);
         this.setState({
-          nameValue: "",
           contentValue: "",
         });
       }
@@ -38,7 +37,6 @@ class Text extends Component {
       if (this.state.contentValue !== "" && this.state.nameValue !== "") {
         this.props.onClick(this.state.nameValue, this.state.contentValue);
         this.setState({
-          nameValue: "",
           contentValue: "",
         });
       }
@@ -46,7 +44,6 @@ class Text extends Component {
     const handleCancel = () => {
       this.props.onCancel();
       this.setState({
-        nameValue: "",
         contentValue: "",
       });
     };
@@ -54,9 +51,9 @@ class Text extends Component {
       (show) ? (
         <div id={style.Text} className="animated zoomIn">
           <div className={style.Subtitle}>name:</div>
-          <input className={style.InputName} placeholder="Add your name..." value={this.state.nameValue} autoFocus type="text" onChange={this.handleChangeName.bind(this)} />
+          <input className={style.InputName} placeholder="Add your name..." value={this.state.nameValue} autoFocus={(this.state.nameValue === "")} type="text" onChange={this.handleChangeName.bind(this)} />
           <div className={style.Subtitle}>content:</div>
-          <textarea className={style.InputContent} value={this.state.contentValue}  onKeyDown={this.handleKeyDown.bind(this)} onChange={this.handleChangeContent.bind(this)} type="text" placeholder="Add comment..." />
+          <textarea className={style.InputContent} value={this.state.contentValue} autoFocus={(this.state.nameValue !== "")} onKeyDown={this.handleKeyDown.bind(this)} onChange={this.handleChangeContent.bind(this)} type="text" placeholder="Add comment..." />
           <div className={style.SubmitButton} onClick={handleClick}>Submit</div>
           <div className={style.CancelButton} onClick={handleCancel}>Cancel</div>
         </div>
