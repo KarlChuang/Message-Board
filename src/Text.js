c909e5638231aaf4d03acbaa2a1ce3ecaeb78b11
KarlChuang <karl0930@gmail.com>
2017-05-08 14:53:05 +0800

    press enter to submit

--- src/Text.js
+++ src/Text.js
@@ -20,6 +20,17 @@ class Text extends Component {
       contentValue: e.target.value,
     });
   }
+  handleKeyDown(e) {
+    if (e.keyCode === 13) {
+      if (this.state.contentValue !== "" && this.state.nameValue !== "") {
+        this.props.onClick(this.state.nameValue, this.state.contentValue);
+        this.setState({
+          nameValue: "",
+          contentValue: "",
+        });
+      }
+    }
+  }
   render() {
     const title = this.props.title;
     const show = this.props.show;
@@ -45,7 +56,7 @@ class Text extends Component {
           <div className={style.Subtitle}>name:</div>
           <input className={style.InputName} placeholder="Add your name..." value={this.state.nameValue} autoFocus type="text" onChange={this.handleChangeName.bind(this)} />
           <div className={style.Subtitle}>content:</div>
-          <textarea className={style.InputContent} value={this.state.contentValue} onChange={this.handleChangeContent.bind(this)} type="text" placeholder="Add comment..." />
+          <textarea className={style.InputContent} value={this.state.contentValue}  onKeyDown={this.handleKeyDown.bind(this)} onChange={this.handleChangeContent.bind(this)} type="text" placeholder="Add comment..." />
           <div className={style.SubmitButton} onClick={handleClick}>Submit</div>
           <div className={style.CancelButton} onClick={handleCancel}>Cancel</div>
         </div>
